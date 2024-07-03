ee98c301966f0dcde4cc5d1997955d8520291a98
CyrilleTual <cyrille.tual@gmail.com>
2024-07-03 20:22:58 +0200

    before service

--- front-client/src/app/card/card.component.ts
+++ front-client/src/app/card/card.component.ts
@@ -8,10 +8,10 @@ import { Component } from '@angular/core';
 
 export class CardComponent {
   cards = [
-    { title: 'Card title 1', text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.' },
-    { title: 'Card title 2', text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.' },
-    { title: 'Card title 3', text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.' },
-    { title: 'Card title 4', text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.' },
-    { title: 'Card title 5', text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.' },
+    { title: 'Card title 1', text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.', src:'https://placeholderimage.eu/api' },
+    { title: 'Card title 2', text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.', src: 'https://placeholderimage.eu/api' },
+    { title: 'Card title 3', text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.', src: 'https://placeholderimage.eu/api' },
+    { title: 'Card title 4', text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.', src: 'https://placeholderimage.eu/api' },
+    { title: 'Card title 5', text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.', src: 'https://placeholderimage.eu/api' },
   ];
 }
