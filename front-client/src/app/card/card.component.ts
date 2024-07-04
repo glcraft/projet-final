9ef99e682d26e2ad8845f0b554c8ca0b626e07eb
CyrilleTual <cyrille.tual@gmail.com>
2024-07-04 20:11:33 +0200

    fully connected to db

--- front-client/src/app/card/card.component.ts
+++ front-client/src/app/card/card.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { SearchService } from '@app/Services/search.service';
-import { dataCards } from '../data/data.js';
+//import { dataCards } from '../data/data.js';
+import { Articles } from '@app/Models/articles';
+import { ArticlesCrudService } from '@app/Services/articles-crud.service';
 
 @Component({
   selector: 'app-card',
@@ -9,23 +11,24 @@ import { dataCards } from '../data/data.js';
 })
 
 export class CardComponent {
-  cards;
+  cards: any;
   searchValue: string = '';
-
-  constructor(private searchService: SearchService) { }
+  dataCards: Array<Articles> = new Array<Articles>();
+  constructor(private searchService: SearchService, private srv: ArticlesCrudService ) { }
 
   ngOnInit() {
+    this.srv.GetAllArticle()
+    .then(a => this.dataCards = a)
+      .then(() => this.cards = this.dataCards) ;
+
+    
     this.searchService.searchValue$.subscribe(value => {
       this.searchValue = value;
-
       // filter cards on title == searchValue
-
-      this.cards = dataCards;
-      this.cards = this.cards.filter(card => card.title.toLowerCase().includes(this.searchValue.toLowerCase()) ||
+      this.cards = this.dataCards;   // on reinitialise les cards
+      this.cards = this.cards.filter(card => card.nom.toLowerCase().includes(this.searchValue.toLowerCase()) ||
         card.resume.toLowerCase().includes(this.searchValue.toLowerCase()));
 
-
-      // Faites quelque chose avec la valeur de recherche, comme filtrer une liste d'articles
     });
   }
 
