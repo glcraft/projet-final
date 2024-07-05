a692a46c2a33aae28a1c7e949617b529af8a130c
Gabin Lefranc <gabin.lefranc@gmail.com>
2024-07-05 11:28:47 +0200

    Ignore self reference

--- back/Rest/App_Start/WebApiConfig.cs
+++ back/Rest/App_Start/WebApiConfig.cs
@@ -14,6 +14,10 @@ namespace Rest
             // Itinéraires de l'API Web
             config.MapHttpAttributeRoutes();
 
+            config.Formatters.JsonFormatter
+                .SerializerSettings
+                .ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
