94d75f4b2b48c8517cbd83e2d1546dba2940515d
ccolomb449 <corentin.colomb@wanadoo.fr>
2024-07-05 12:39:49 +0200

    suppr de commandes sur angular + api de filtre v1

--- back/Rest/Controllers/ArticlesController.cs
+++ back/Rest/Controllers/ArticlesController.cs
@@ -8,6 +8,7 @@ using System.Web.Http;
 using System.Web.Http.Cors;
 using System.Web.Routing;
 using System.Security.Principal;
+using Rest.Models;
 
 namespace Rest.Controllers
 {
@@ -41,5 +42,53 @@ namespace Rest.Controllers
         //    ctx.Entry(article).State = System.Data.Entity.EntityState.Modified;
         //    ctx.SaveChanges();
         //}
+        [HttpPost]
+        [Route("api/articles/search")]
+        public IEnumerable<Articles> Search([FromBody] ArticleFilter filter)
+        {
+            var ctx = new ProjetFinalEntities().Articles.Include("TagsPriv").AsQueryable();
+
+            ctx = ctx.Where(a => a.archive == null);
+
+            if (!string.IsNullOrEmpty(filter.Nom))
+            {
+                ctx = ctx.Where(a => a.nom.Contains(filter.Nom));
+            }
+
+            if (filter.PrixMin >= 0 && filter.PrixMax >= 0 && filter.PrixMin <= filter.PrixMax)
+            {
+                ctx = ctx.Where(a => a.prix >= filter.PrixMin && a.prix <= filter.PrixMax);
+            }
+            else if (filter.PrixMin >= 0)
+            {
+                ctx = ctx.Where(a => a.prix >= filter.PrixMin);
+            }
+            else if (filter.PrixMax >= 0)
+            {
+                ctx = ctx.Where(a => a.prix <= filter.PrixMax);
+            }
+
+            if (filter.Tags != null && filter.Tags.Any())
+            {
+                ctx = ctx.Where(a => filter.Tags.Any(tag => a.Tags.Any(t => t == tag)));
+            }
+
+            if (!string.IsNullOrEmpty(filter.Marque))
+            {
+                ctx = ctx.Where(a => a.marque.Contains(filter.Marque));
+            }
+
+            return ctx.AsEnumerable();
+        }
+
+        [HttpGet]
+        [Route("api/articles/prix")]
+        public IHttpActionResult GetPrix()
+        {
+            var PrixMin = new ProjetFinalEntities().Articles.Where(a => a.archive == null).Min(a => a.prix);
+            var PrixMax = new ProjetFinalEntities().Articles.Where(a => a.archive == null).Max(a => a.prix);
+
+            return Ok(new { PrixMin = PrixMin, PrixMax = PrixMax });
+        }
     }
 }
---
+++ back/Rest/Models/ArticleFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rest.Models
+{
+    public class ArticleFilter
+    {
+        public string Nom { get; set; }
+        public int PrixMin { get; set; }
+        public int PrixMax { get; set; }
+        public string[] Tags { get; set; }
+        public string Marque { get; set; }
+    }
+}
