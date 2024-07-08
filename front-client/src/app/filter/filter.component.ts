cae3b67e1be46afaac1f67b02d79c784e6b15737
Gabin Lefranc <gabin.lefranc@gmail.com>
2024-07-08 18:31:59 +0200

    brand new search angine

--- front-client/src/app/filter/filter.component.ts
+++ front-client/src/app/filter/filter.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
 import { Articles } from '@app/Models/articles';
-import { ArticlesCrudService } from '@app/Services/articles-crud.service';
 import { FilterService } from '@app/Services/filter.service';
+
+interface SearchCriteria {
+  priceMin?: number;
+  priceMax?: number;
+  nom?: string;
+  marque?: string;
+  tags?: string;
+}
 
 @Component({
   selector: 'app-filter',
@@ -9,45 +17,85 @@ import { FilterService } from '@app/Services/filter.service';
   styleUrls: ['./filter.component.css']
 })
 export class FilterComponent {
-  searchCriteria = {
-    priceMin: 0,
-    priceMax: 0,
-    nom: '',
-    marque: '',
-    tags: ''
-  };
-
+  searchCriteria: SearchCriteria = {};
+  prices: Array<number> | null = null;
 
   articles: Articles[] = [];
   filteredArticles: Array<Articles> = new Array<Articles>();
-  constructor(private srv: FilterService) { };
 
-  ngOnInit() {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private srv: FilterService) { 
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.searchCriteria = {
+        priceMin: params['priceMin'],
+        priceMax: params['priceMax'],
+        nom: params['nom'],
+        marque: params['marque'],
+        tags: params['tags']
+      };
+      (!this.prices 
+        ? this.srv.getPrices().then(p => this.prices = p) 
+        : new Promise<void>((resolve, reject) => resolve())
+      ).then(() => {
+        if (!this.searchCriteria.priceMin)
+          this.searchCriteria.priceMin = this.prices[0];
+        if (!this.searchCriteria.priceMax)
+          this.searchCriteria.priceMax = this.prices[1];
+      });
+    });
+  };
+
+  async ngOnInit() {
+    this.updateFilteredArticles(this.transformSearchOptions());
   }
 
   onSearch() {
-    const { priceMin, priceMax, nom, marque, tags } = this.searchCriteria;
-    const tagsArray = tags.split(',').map(tag => tag.trim().toLowerCase());
+    this.updateFilteredArticles(this.transformSearchOptions());
 
-    if (tagsArray.length === 1 && tagsArray[0] === '') { tagsArray.pop(); }
-
-    let toSearch = {
-      "Nom": this.searchCriteria.nom,
-      "Prix": [this.searchCriteria.priceMin, this.searchCriteria.priceMax],
-      "Tags": tagsArray,
-      "Marque": this.searchCriteria.marque
-    }
-    console.log("recherche :",toSearch);
-    this.getFilteredArticles(toSearch );
+    this.router.navigate(
+      [], 
+      {
+        relativeTo: this.activatedRoute,
+        queryParams: this.formatCriteria(), 
+        queryParamsHandling: 'merge', // remove to replace all query params by provided
+      }
+    );
   }
 
-  async getFilteredArticles(toSearch: any) {
+  async updateFilteredArticles(toSearch: any) {
     try {
       const resp: Array<Articles> = await this.srv.FilterArticles(toSearch);
       this.filteredArticles = resp;
-      console.log(resp);
     } catch (error) {
       console.error('Erreur lors de la récupération des articles:', error);
+    }
+  }
+
+  transformSearchOptions(){
+    return {
+      "Nom": this.searchCriteria.nom,
+      "Prix": this.searchCriteria.priceMin && this.searchCriteria.priceMax ? [this.searchCriteria.priceMin, this.searchCriteria.priceMax]: undefined,
+      "Tags": this.searchCriteria.tags ? this.searchCriteria.tags.split(',').map(tag => tag.trim()): undefined,
+      "Marque": this.searchCriteria.marque,
+      Limit: 20
+    }
+  }
+
+  formatCriteria(): SearchCriteria {
+    let prices = [undefined, undefined];
+    if (this.prices) {
+      if (this.searchCriteria.priceMin && this.searchCriteria.priceMin != this.prices[0]) {
+        prices[0] = this.searchCriteria.priceMin;
+      }
+      if (this.searchCriteria.priceMax && this.searchCriteria.priceMax != this.prices[1]) {
+        prices[1] = this.searchCriteria.priceMax;
+      }
+    }
+    return {
+      priceMin: prices[0],
+      priceMax: prices[1],
+      nom: !!this.searchCriteria.nom ? this.searchCriteria.nom : undefined,
+      marque: !!this.searchCriteria.marque ? this.searchCriteria.marque : undefined,
+      tags: !!this.searchCriteria.tags ? this.searchCriteria.tags : undefined
     }
   }
 }
