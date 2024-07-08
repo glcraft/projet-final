5e9d18a0702e9a21b8395be3d8e7827389bfe327
Gabin Lefranc <gabin.lefranc@gmail.com>
2024-07-08 11:40:25 +0200

    dynamic load carousel

--- front-client/src/app/carousel/carousel.component.ts
+++ front-client/src/app/carousel/carousel.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Articles } from '@app/Models/articles';
+import { FilterService } from '@app/Services/filter.service';
 
 @Component({
   selector: 'app-carousel',
@@ -6,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent {
+  articles: Array<Articles> = [];
+  constructor(private http: HttpClient, private searchSrv: FilterService) { }
 
+  async ngOnInit() {
+    this.articles = await this.searchSrv.FilterArticles({Limit: 3});
+  }
 }
